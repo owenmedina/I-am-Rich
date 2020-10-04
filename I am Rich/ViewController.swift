fef0eef97292c7faa5039433bb83ce0177f8265e
Owen Medina <owensmedi@gmail.com>
2020-10-04 15:46:00 -0700

    First commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Owen Medina on 10/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
